b84c57cf1e4bdaf21ea76e43dd87a26b93d5deca
Samrat <sen.samrat.samrat@gmail.com>
2024-05-31 19:49:00 +0530

    First Commit and Push to Github for Brandpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Herosection from "./components/Hero";
+import Navigation from "./components/Navigation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    return <div>
+        <Navigation />
+        <Herosection />
     </div>
-  );
-}
+};
 
 export default App;
